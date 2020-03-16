fb00b70bf8994081c30d70c3294e25194af6dd0d
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-03-16 15:13:47 +0100

    Made an Atom implementation of Function2d

---
+++ parser/AtomFunction2d.java
@@ -0,0 +1,37 @@
+package parser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import main.Function2d;
+import main.Vector2d;
+
+public class AtomFunction2d implements Function2d {
+
+	Atom function;
+	Atom derivative_x;
+	Atom derivative_y;
+	Map<String, Double> map;
+	
+	public AtomFunction2d(Atom function) {
+		this.function = function;
+		derivative_x = function.derivate("x");
+		derivative_y = function.derivate("y");
+		map = new HashMap<String, Double>();
+	}
+	
+	@Override
+	public Vector2d gradient(Vector2d p) {
+		map.put("x", p.get_x());
+		map.put("y", p.get_y());
+		return new Vector2d(derivative_x.apply(map), derivative_y.apply(map));
+	}
+
+	@Override
+	public double evaluate(double x, double y) {
+		map.put("x", x);
+		map.put("y", y);
+		return function.apply(map);
+	}
+
+}
