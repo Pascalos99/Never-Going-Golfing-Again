d40f04944175f5b4aa780e1d7fe73f7fabec690f
SamueleTorregrossa <samuele_t95@hotmail.com>
2020-03-30 21:54:36 +0200

    DEPTH BUFFER TEST(?)

--- core/src/com/mygdx/game/CrazyPutting.java
+++ core/src/com/mygdx/game/CrazyPutting.java
@@ -326,6 +326,7 @@ public class CrazyPutting implements ApplicationListener {
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glEnable(GL20.GL_DEPTH_TEST);
 
         previous_time = world_physics.frameStep(previous_time);
 
