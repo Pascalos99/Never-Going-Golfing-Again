a1546321b255c0cb520894fb29b20d52d65d80dd
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-04-13 15:49:03 +0200

    added course width and height to ArrayFunction

--- core/src/com/mygdx/game/ArrayFunction2d.java
+++ core/src/com/mygdx/game/ArrayFunction2d.java
@@ -3,5 +3,13 @@ package com.mygdx.game;
 public interface ArrayFunction2d extends Function2d {
 	
 	public double[][] getArray();
+
+	public default int getCourseWidth() {
+		return getArray().length;
+	}
+
+	public default int getCourseHeight() {
+		return getArray()[0].length;
+	}
 	
 }
