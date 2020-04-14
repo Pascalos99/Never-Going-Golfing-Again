d2d7da9cbf5933ef502a6149cc7ef681122ec375
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-04-14 20:13:03 +0200

    fixed shot velocity variable

--- core/src/com/mygdx/game/Variables.java
+++ core/src/com/mygdx/game/Variables.java
@@ -80,7 +80,8 @@ public final class Variables {
 	//PHYSICS
 	public static double VELOCITY_CUTTOFF = 0.05d; // in m/s
 	public static double GRADIENT_CUTTOFF = 1d; // no clue what unit this is
-	public static double SHOT_VELOCITY = 4d;
-	public static double SHOT_VELOCITY_INCREASE = 0.01 * SHOT_VELOCITY;
+	public static double MAX_SHOT_VELOCITY = 4d;
+	public static double SHOT_VELOCITY = MAX_SHOT_VELOCITY/2d;
+	public static double SHOT_VELOCITY_INCREASE = 0.01 * MAX_SHOT_VELOCITY;
 	public static double VELOCITY_FACTOR_FROM_BOUNCING_AGAINST_WALL = 0.9;
 }
