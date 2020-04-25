59ab56cbbc860c8455695e7816f61d8e1a4a68c6
DQBOTheFirst <denn.mertens@outlook.com>
2020-04-26 00:18:06 +0200

    Fixed height scaling to properly match horizontal proportions. Note: Because of this, our functions need to be scaled... f(x, y) / 7.692307692307692

---
+++ core/src/com/mygdx/game/AIUtils.java
@@ -0,0 +1,22 @@
+package com.mygdx.game;
+
+import java.util.List;
+
+public final class AIUtils {
+
+    Vector2d findLowestGradient(){
+        Vector2d xy = null;
+        double total_gradient = 0;
+
+        return null;
+    }
+
+    double unfoldDistance(Vector2d a, Vector2d b){
+        return 0;
+    }
+
+    List<Vector2d> getPointsWithGradient(Vector2d gradient){
+        return null;
+    }
+
+}
