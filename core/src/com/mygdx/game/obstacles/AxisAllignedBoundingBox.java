ad894564303050b060cb143fd709f3de7d56b1d1
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-06-11 19:31:11 +0200

    Hey Dennis, wanna help?

---
+++ core/src/com/mygdx/game/obstacles/AxisAllignedBoundingBox.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.obstacles;
+
+public class AxisAllignedBoundingBox {
+
+
+
+}
---
+++ core/src/com/mygdx/game/obstacles/Static3DphysicsObject.java
@@ -0,0 +1,7 @@
+package com.mygdx.game.obstacles;
+
+public abstract class Static3DphysicsObject {
+
+    public abstract AxisAllignedBoundingBox getBoundingBox();
+
+}
