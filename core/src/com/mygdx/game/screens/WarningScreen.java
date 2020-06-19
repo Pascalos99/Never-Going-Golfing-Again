b0328c9d79ae8742a8533cbf7f121c51b6dbd01e
A-gamil <alaagamil5264@gmail.com>
2020-06-19 02:00:37 +0200

    finished up warning screen, added TODO for not changing wall height and camera angle when game resets

--- core/src/com/mygdx/game/screens/WarningScreen.java
+++ core/src/com/mygdx/game/screens/WarningScreen.java
@@ -3,22 +3,24 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.game.courses.GameInfo;
 
-import static com.mygdx.game.utils.Variables.MENU_BKG;
-import static com.mygdx.game.utils.Variables.MENU_SKIN;
+import static com.mygdx.game.utils.Variables.*;
 
 public class WarningScreen implements Screen {
     private Menu parent;
     private Stage stage;
     private Label msg;
 
-    public WarningScreen(Menu parent){
-        this.parent=parent;
+    public WarningScreen(Menu menu){
+        this.parent=menu;
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
@@ -32,12 +34,33 @@ public class WarningScreen implements Screen {
         table.add(overall);
 
 
-        msg= new Label("",MENU_SKIN);
+        msg= new Label("EITHER THE GOAL OR THE FLAG ARE IN WATER",MENU_SKIN);
         overall.add(msg);
 
         TextButton playAnyway=new TextButton("Play Anyway",MENU_SKIN);
         TextButton obstacleSelect=new TextButton("Fix Via Obstacle", MENU_SKIN);
+        playAnyway.addListener(new ChangeListener() {
 
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+                parent.changeScreen(Menu.PLAY);
+            }
+        });
+        obstacleSelect.addListener(new ChangeListener() {
+
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+
+                parent.changeScreen(Menu.CUSTOMIZE_OBSTACLES);
+            }
+        });
+        Table buttons= new Table();
+        buttons.add(obstacleSelect).pad(30,0,0,30);
+        buttons.add(playAnyway).pad(30,30,0,0);
+
+        overall.row().pad(10,0,10,0);
+        overall.add(buttons);
 
 
 
