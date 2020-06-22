53eae39c510c90fce17886caddf63a14f62607e9
Pascalos99 <56164915+Pascalos99@users.noreply.github.com>
2020-06-22 03:13:44 +0200

    reverted revert for wanted changes

--- core/src/com/mygdx/game/bots/tree_search/HeuristicFunction.java
+++ core/src/com/mygdx/game/bots/tree_search/HeuristicFunction.java
@@ -4,15 +4,9 @@ package com.mygdx.game.bots.tree_search;
 public interface HeuristicFunction {
     /**
      * @param node the simulated node for which the h-value must be calculated
-     * @return the absolute heuristic value corresponding to the simulation results from {@code node}
+     * @return the heuristic value corresponding to the simulation results from {@code node}, the higher this return value,
+     *         the better the {@code node} is.
      */
     double calculate(Node node);
-
-    // TODO implementation of heuristic where h decreases as depth increases according to a minimum_improvement factor:
-    /** double factor = 1 + percentage_improvement;
-     *  if (depth <= 0) h = -distance(current, goal)
-     *  else h = parent.h * factor + distance(previous, goal) - distance(current, goal)
-     *
-     */
 
 }
